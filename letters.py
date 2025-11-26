185eeb6f76af9eee98c1a6e9c5a87e888a8c0c59
Emma Gershman <gershman.e@northeastern.edu>
2025-11-25 19:18:27 -0500

    create file

---
+++ letters.py
@@ -0,0 +1,2 @@
+URL = 'https://newsinteractives.cbc.ca/longform/remember-that-i-love-you-a-soldiers-letters-to-his-sweetheart/'
+URL2 = 'https://heartofthecivilwar.org/civil-war-love-letters-tales-of-love-and-loss/'
