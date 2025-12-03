3382fbffcf0aa25da06a8a3db67e1c8bf5d55f25
Emma Gershman <gershman.e@northeastern.edu>
2025-12-03 16:25:59 -0500

    add data

--- letters.py
+++ letters.py
@@ -1,2 +1,14 @@
 URL = 'https://newsinteractives.cbc.ca/longform/remember-that-i-love-you-a-soldiers-letters-to-his-sweetheart/'
-URL2 = 'https://heartofthecivilwar.org/civil-war-love-letters-tales-of-love-and-loss/'
+URL2 = 'https://blog.forceswarrecords.com/love-letter-from-a-world-war-one-soldier/'
+URL3 = 'https://www.iwm.org.uk/history/letters-to-loved-ones'
+
+import requests
+from bs4 import BeautifulSoup
+
+def retrieve_webpage(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        html_content = response.text
+    else:
+        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+    return response
