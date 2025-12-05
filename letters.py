afa9ec8dd4bca16d760f4ae2ce598a6bad47c433
Emma Gershman <gershman.e@northeastern.edu>
2025-12-04 19:42:42 -0500

    Update letters.py

--- letters.py
+++ letters.py
@@ -28,14 +28,56 @@ URL3 = 'https://www.iwm.org.uk/history/letters-to-loved-ones'
 
 # self.data dictionary attribute and class TextFramework (Emma)
 """
+from collections import Counter, defaultdict
+import random as rnd
+import matplotlib.pyplot as plt
 
-import requests
-from bs4 import BeautifulSoup
+class Letters:
 
-def retrieve_webpage(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        html_content = response.text
-    else:
-        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
-    return response
+    def __init__(self):
+        """ Constructor to initialize state """
+
+        # Where all the data extracted from the loaded documents is stored
+        self.data = defaultdict(dict)
+
+    @staticmethod
+    def default_parser(filename):
+        """ For processing plain text files (.txt) """
+        results = {
+            'wordcount': Counter("to be or not to be".split(" ")),
+            'numwords': rnd.randrange(10, 50)
+        }
+
+        print("Parsed ", filename, ": ", results)
+        return results
+
+    def load_text(self, filename, label=None, parser=None):
+        """ Register a text document with the framework.
+         Extract and store data to be used later in our visualizations. """
+        if parser is None:
+            results = Letters.default_parser(filename)
+        else:
+            results = parser(filename)
+
+        # Use filename for the label if none is provided
+        if label is None:
+            label = filename
+
+        # Store the results for that ONE document into self.data
+        # For example, document A:  numwords=10,  document B: numwords=20
+        # For A, the results are: {numwords:10}, for B: {numwords:20}
+        # This gets stored as: {numwords: {A:10, B:20}}
+
+        for k, v in results.items():
+            self.data[k][label] = v
+
+
+    def compare_num_words(self):
+        """ A very simplistic visualization that creates
+        a bar chart comparing num words for each text file
+        For HW7, I expect much more interesting visualizations """
+
+        numwords = self.data['numwords']
+        for label, nw in numwords.items():
+            plt.bar(label, nw)
+        plt.show()
